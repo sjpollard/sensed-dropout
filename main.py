8359f3fbe4e90265824095a2abcb55e18e210f00
sjpollard <samjosephpollard@gmail.com>
2023-10-17 19:29:59 +0100

    Introduced SSPOR as an unguided alternative. Pulled out fitting of these models into functions

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from sklearn.metrics import accuracy_score
 
 import pysensors as ps
 from pysensors.classification import SSPOC
+from pysensors.reconstruction import SSPOR
 
 from patchify import patchify, unpatchify
 
@@ -23,16 +24,38 @@ def get_CIFAR10(size=-1, train=True):
     X, y = next(iter(dataloader))
     return X.numpy().squeeze(), y.numpy()
 
-def show_basis(model: SSPOC, height: int, width: int, n: int = 100):
+def fit_SSPOC(X_train, y_train, n_basis_modes, n_sensors, l1_penalty):
+    n, height, width = X_train.shape
+
+    basis = ps.basis.SVD(n_basis_modes=n_basis_modes)
+
+    model = SSPOC(basis=basis, n_sensors=n_sensors, l1_penalty=l1_penalty)
+    model.fit(np.reshape(X_train, (n, height * width)), y_train)
+    
+    print(f'{len(model.get_selected_sensors())} sensors selected out of {height * width}')
+    return model
+
+def fit_SSPOR(X_train, n_basis_modes, n_sensors):
+    n, height, width = X_train.shape
+
+    basis = ps.basis.SVD(n_basis_modes=n_basis_modes)
+
+    model = SSPOR(basis=basis, n_sensors=n_sensors)
+    model.fit(np.reshape(X_train, (n, height * width)))
+    
+    print(f'{len(model.get_selected_sensors())} sensors selected out of {height * width}')
+    return model
+
+def show_basis(model: SSPOC | SSPOR, height: int, width: int, n: int = 100):
     modes = model.basis.matrix_representation().shape[1]
     ts.show(np.reshape(model.basis.matrix_representation().T, (modes, height, width))[:n], mode='grayscale')
 
-def show_sensors(model: SSPOC, height: int, width: int):
+def show_sensors(model: SSPOC | SSPOR, height: int, width: int):
     sensors = np.zeros(height * width)
     np.put(sensors, model.get_selected_sensors(), 1)
     ts.show(np.reshape(sensors, (height, width)), mode='grayscale')
 
-def print_accuracies(model: SSPOC, X_train, y_train, n, height, width):
+def print_accuracies(model: SSPOC | SSPOR, X_train, y_train, n, height, width):
     X_test, y_test = get_CIFAR10(train=False)
 
     y_pred = model.predict(np.reshape(X_train, (n, height * width))[:,model.selected_sensors])
@@ -46,20 +69,16 @@ def main():
 
     n, height, width = X_train.shape
 
-    n_basis_modes = 100
-    l1_penalty = 0.1
-    n_sensors = 512
+    model_r = fit_SSPOR(X_train, 100, 256)
+    model_c = fit_SSPOC(X_train, y_train, 100, 256, 0.0000005)
 
-    basis = ps.basis.SVD(n_basis_modes=n_basis_modes)
+    """ show_basis(model, height, width) """
+    show_sensors(model_r, height, width)
+    show_sensors(model_c, height, width)
+    """ print_accuracies(model, X_train, y_train, n, height, width) """
 
-    model = SSPOC(basis=basis, n_sensors=n_sensors, l1_penalty=l1_penalty)
-    model.fit(np.reshape(X_train, (n, height * width)), y_train)
-    
-    print(f'{len(model.get_selected_sensors())} selected out of {height * width}')
 
-    show_basis(model, height, width)
-    show_sensors(model, height, width)
-    print_accuracies(model, X_train, y_train, n, height, width)
+
 
 if __name__ == "__main__":
     main()
