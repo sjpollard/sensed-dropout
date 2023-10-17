111802a0ff4ccc61938468caa57da11f7059487e
sjpollard <samjosephpollard@gmail.com>
2023-10-17 18:41:39 +0100

    Pulled out train & test inference into function

--- main.py
+++ main.py
@@ -23,33 +23,16 @@ def get_CIFAR10(size=-1, train=True):
     X, y = next(iter(dataloader))
     return X.numpy().squeeze(), y.numpy()
 
-def show_basis(model: SSPOC, height: int, width: int):
+def show_basis(model: SSPOC, height: int, width: int, n: int = 100):
     modes = model.basis.matrix_representation().shape[1]
-    ts.show(np.reshape(model.basis.matrix_representation().T, (modes, height, width)), mode='grayscale')
+    ts.show(np.reshape(model.basis.matrix_representation().T, (modes, height, width))[:n], mode='grayscale')
 
 def show_sensors(model: SSPOC, height: int, width: int):
     sensors = np.zeros(height * width)
     np.put(sensors, model.get_selected_sensors(), 1)
     ts.show(np.reshape(sensors, (height, width)), mode='grayscale')
 
-def main():
-    X_train, y_train = get_CIFAR10()
-
-    n, height, width = X_train.shape
-
-    n_basis_modes = 20
-    l1_penalty = 0.000005
-
-    basis = ps.basis.SVD(n_basis_modes=n_basis_modes)
-
-    model = SSPOC(basis=basis, l1_penalty=l1_penalty)
-    model.fit(np.reshape(X_train, (n, height * width)), y_train)
-    
-    print(f'{len(model.get_selected_sensors())} selected out of {height * width}')
-
-    """ show_basis(model, height, width) """
-    """ show_sensors(model, height, width) """
-
+def print_accuracies(model: SSPOC, X_train, y_train, n, height, width):
     X_test, y_test = get_CIFAR10(train=False)
 
     y_pred = model.predict(np.reshape(X_train, (n, height * width))[:,model.selected_sensors])
@@ -58,7 +41,25 @@ def main():
     y_pred = model.predict(np.reshape(X_test, (X_test.shape[0], height * width))[:,model.selected_sensors])
     print(f'Test accuracy: {accuracy_score(y_test, y_pred) * 100}%')
 
+def main():
+    X_train, y_train = get_CIFAR10()
 
+    n, height, width = X_train.shape
+
+    n_basis_modes = 100
+    l1_penalty = 0.1
+    n_sensors = 512
+
+    basis = ps.basis.SVD(n_basis_modes=n_basis_modes)
+
+    model = SSPOC(basis=basis, n_sensors=n_sensors, l1_penalty=l1_penalty)
+    model.fit(np.reshape(X_train, (n, height * width)), y_train)
+    
+    print(f'{len(model.get_selected_sensors())} selected out of {height * width}')
+
+    show_basis(model, height, width)
+    show_sensors(model, height, width)
+    print_accuracies(model, X_train, y_train, n, height, width)
 
 if __name__ == "__main__":
     main()
